2dc5b56eecc3468d7ad2fa308795b0b65d1f7ca4
zww4855 <zww4855@gmail.com>
2022-08-24 09:59:59 -0400

    Begin CCD eqns in spin orbital form

---
+++ examples/full_cc_codes/ccd.py
@@ -0,0 +1,197 @@
+"""
+A full working spin-orbital CCSD code generated with pdaggerq
+
+If you want to run the example here you should install pyscf openfermion and openfermion-pyscf
+The actual CCSD code (ccsd_energy, singles_residual, doubles_residual, kernel)
+do not depend on those packages but you gotta get integrals frome somehwere.
+
+We also check the code by using pyscfs functionality for generating spin-orbital
+t-amplitudes from RCCSD.  the main() function is fairly straightforward.
+"""
+import numpy as np
+from numpy import einsum
+
+
+def ccsd_energy(t1, t2, f, g, o, v):
+    """
+    < 0 | e(-T) H e(T) | 0> :
+
+    :param f:
+    :param g:
+    :param t1:
+    :param t2:
+    :param o:
+    :param v:
+    :return:
+    """
+
+    #	 -0.5000 <j,i||j,i>
+    energy += -0.5 * einsum('jiji', g[o, o, o, o])
+
+    #	  0.2500 <j,i||a,b>*t2(a,b,j,i)
+    energy += 0.25 * einsum('jiab,abji', g[o, o, v, v], t2)
+
+
+    return energy
+
+
+
+
+def doubles_residual(t1, t2, f, g, o, v):
+    """
+     < 0 | m* n* f e e(-T) H e(T) | 0>
+
+    :param f:
+    :param g:
+    :param t1:
+    :param t2:
+    :param o:
+    :param v:
+    :return:
+    """
+
+#      1.0000 <a,b||i,j>
+    doubles_res +=  1.000000000000000 * einsum('abij->abij', g[v, v, o, o])
+
+#      0.5000 <l,k||i,j>*t2(a,b,l,k)
+    doubles_res +=  0.500000000000000 * einsum('lkij,ablk->abij', g[o, o, o, o], t2)
+
+#      1.0000 P(i,j)*P(a,b)<k,a||c,j>*t2(c,b,i,k)
+    contracted_intermediate =  1.000000000000000 * einsum('kacj,cbik->abij', g[o, v, v, o], t2)
+    doubles_res +=  1.00000 * contracted_intermediate + -1.00000 * einsum('abij->abji', contracted_intermediate)  + -1.00000 * einsum('abij->baij', contracted_intermediate)  +  1.00000 * einsum('abij->baji', contracted_intermediate)
+
+#      0.5000 <a,b||c,d>*t2(c,d,i,j)
+    doubles_res +=  0.500000000000000 * einsum('abcd,cdij->abij', g[v, v, v, v], t2)
+
+#     -0.5000 P(i,j)<l,k||c,d>*t2(c,d,j,k)*t2(a,b,i,l)
+    contracted_intermediate = -0.500000000000000 * einsum('lkcd,cdjk,abil->abij', g[o, o, v, v], t2, t2, optimize=['einsum_path', (0, 1), (0, 1)])
+    doubles_res +=  1.00000 * contracted_intermediate + -1.00000 * einsum('abij->abji', contracted_intermediate)
+
+#      0.2500 <l,k||c,d>*t2(c,d,i,j)*t2(a,b,l,k)
+    doubles_res +=  0.250000000000000 * einsum('lkcd,cdij,ablk->abij', g[o, o, v, v], t2, t2, optimize=['einsum_path', (0, 1), (0, 1)])
+
+#     -0.5000 <l,k||c,d>*t2(c,a,l,k)*t2(d,b,i,j)
+    doubles_res += -0.500000000000000 * einsum('lkcd,calk,dbij->abij', g[o, o, v, v], t2, t2, optimize=['einsum_path', (0, 1), (0, 1)])
+
+#      1.0000 P(i,j)<l,k||c,d>*t2(c,a,j,k)*t2(d,b,i,l)
+    contracted_intermediate =  1.000000000000000 * einsum('lkcd,cajk,dbil->abij', g[o, o, v, v], t2, t2, optimize=['einsum_path', (0, 1), (0, 1)])
+    doubles_res +=  1.00000 * contracted_intermediate + -1.00000 * einsum('abij->abji', contracted_intermediate)
+
+#     -0.5000 <l,k||c,d>*t2(c,a,i,j)*t2(d,b,l,k)
+    doubles_res += -0.500000000000000 * einsum('lkcd,caij,dblk->abij', g[o, o, v, v], t2, t2, optimize=['einsum_path', (0, 2), (0, 1)])
+
+    return doubles_res
+
+def kernel(t1, t2, fock, g, o, v, e_ai, e_abij, max_iter=100, stopping_eps=1.0E-8,
+           diis_size=None, diis_start_cycle=4):
+
+    # initialize diis if diis_size is not None
+    # else normal scf iterate
+    if diis_size is not None:
+        from diis import DIIS
+        diis_update = DIIS(diis_size, start_iter=diis_start_cycle)
+        t1_dim = t1.size
+        old_vec = np.hstack((t1.flatten(), t2.flatten()))
+
+    fock_e_ai = np.reciprocal(e_ai)
+    fock_e_abij = np.reciprocal(e_abij)
+    old_energy = ccsd_energy(t1, t2, fock, g, o, v)
+    for idx in range(max_iter):
+
+        singles_res = singles_residual(t1, t2, fock, g, o, v) + fock_e_ai * t1
+        doubles_res = doubles_residual(t1, t2, fock, g, o, v) + fock_e_abij * t2
+
+        new_singles = singles_res * e_ai
+        new_doubles = doubles_res * e_abij
+
+        # diis update
+        if diis_size is not None:
+            vectorized_iterate = np.hstack(
+                (new_singles.flatten(), new_doubles.flatten()))
+            error_vec = old_vec - vectorized_iterate
+            new_vectorized_iterate = diis_update.compute_new_vec(vectorized_iterate,
+                                                                 error_vec)
+            new_singles = new_vectorized_iterate[:t1_dim].reshape(t1.shape)
+            new_doubles = new_vectorized_iterate[t1_dim:].reshape(t2.shape)
+            old_vec = new_vectorized_iterate
+
+        current_energy = ccsd_energy(new_singles, new_doubles, fock, g, o, v)
+        delta_e = np.abs(old_energy - current_energy)
+
+        if delta_e < stopping_eps:
+            return new_singles, new_doubles
+        else:
+            t1 = new_singles
+            t2 = new_doubles
+            old_energy = current_energy
+            print("\tIteration {: 5d}\t{: 5.15f}\t{: 5.15f}".format(idx, old_energy, delta_e))
+    else:
+        print("Did not converge")
+        return new_singles, new_doubles
+
+
+def main():
+    from itertools import product
+    import pyscf
+    import openfermion as of
+    from openfermion.chem.molecular_data import spinorb_from_spatial
+    from openfermionpyscf import run_pyscf
+    from pyscf.cc.addons import spatial2spin
+    import numpy as np
+
+
+    basis = 'cc-pvdz'
+    mol = pyscf.M(
+        atom='H 0 0 0; B 0 0 {}'.format(1.6),
+        basis=basis)
+
+    mf = mol.RHF().run()
+    mycc = mf.CCSD().run()
+    print('CCSD correlation energy', mycc.e_corr)
+
+    molecule = of.MolecularData(geometry=[['H', (0, 0, 0)], ['B', (0, 0, 1.6)]],
+                                basis=basis, charge=0, multiplicity=1)
+    molecule = run_pyscf(molecule, run_ccsd=True)
+    oei, tei = molecule.get_integrals()
+    norbs = int(mf.mo_coeff.shape[1])
+    occ = mf.mo_occ
+    nele = int(sum(occ))
+    nocc = nele // 2
+    assert np.allclose(np.transpose(mycc.t2, [1, 0, 3, 2]), mycc.t2)
+
+    soei, stei = spinorb_from_spatial(oei, tei)
+    astei = np.einsum('ijkl', stei) - np.einsum('ijlk', stei)
+
+    # put in physics notation. OpenFermion stores <12|2'1'>
+    gtei = astei.transpose(0, 1, 3, 2)
+
+    eps = np.kron(molecule.orbital_energies, np.ones(2))
+    n = np.newaxis
+    o = slice(None, 2 * nocc)
+    v = slice(2 * nocc, None)
+
+    e_abij = 1 / (-eps[v, n, n, n] - eps[n, v, n, n] + eps[n, n, o, n] + eps[
+        n, n, n, o])
+    e_ai = 1 / (-eps[v, n] + eps[n, o])
+
+    fock = soei + np.einsum('piiq->pq', astei[:, o, o, :])
+    hf_energy = 0.5 * np.einsum('ii', (fock + soei)[o, o])
+    hf_energy_test = 1.0 * einsum('ii', fock[o, o]) -0.5 * einsum('ijij', gtei[o, o, o, o])
+    print(hf_energy_test, hf_energy)
+    assert np.isclose(hf_energy + molecule.nuclear_repulsion, molecule.hf_energy)
+
+    g = gtei
+    nsvirt = 2 * (norbs - nocc)
+    nsocc = 2 * nocc
+    t1f, t2f = kernel(np.zeros((nsvirt, nsocc)), np.zeros((nsvirt, nsvirt, nsocc, nsocc)), fock, g, o, v, e_ai, e_abij)
+    print(ccsd_energy(t1f, t2f, fock, g, o, v) - hf_energy)
+
+    t1f, t2f = kernel(np.zeros((nsvirt, nsocc)), np.zeros((nsvirt, nsvirt, nsocc, nsocc)), fock, g, o, v, e_ai, e_abij,
+                      diis_size=8, diis_start_cycle=4)
+    print(ccsd_energy(t1f, t2f, fock, g, o, v) - hf_energy)
+
+
+
+
+if __name__ == "__main__":
+    main()
